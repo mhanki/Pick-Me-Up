a534b29bd0a35ef719b7955bc8ec8504cc888176
mhanki <meike.hankewicz@protonmail.com>
2023-05-04 15:00:03 +0200

    add map region and markers

--- src/features/map/components/Search.js
+++ src/features/map/components/Search.js
@@ -1,10 +1,14 @@
-import { useContext, useState, useEffect } from "react";
-import styled from "styled-components/native";
-import { Searchbar } from "react-native-paper";
-import { LocationContext } from "../../../services/location/location.context";
+import { useContext, useState, useEffect } from 'react';
+import styled from 'styled-components/native';
+import { Searchbar } from 'react-native-paper';
+import { LocationContext } from '../../../services/location/location.context';
 
 const SearchContainer = styled.View`
   padding: ${({theme}) => theme.space[3]};
+  position: absolute;
+  z-index: 999;
+  top: 40px;
+  width: 100%;
 `;
 
 export const Search = () => {
@@ -14,11 +18,12 @@ export const Search = () => {
   useEffect(() => {
     setSearchKeyword(keyword);
   }, [keyword]);
-
+  
   return (
     <SearchContainer>
       <Searchbar
         placeholder="Search for a location"
+        icon="map"
         value={searchKeyword}
         onSubmitEditing={() => {
           search(searchKeyword);
--- src/infrastructure/navigation/AppNavigator.js
+++ src/infrastructure/navigation/AppNavigator.js
@@ -2,7 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
 import { RestaurantsNavigator } from "./RestaurantsNavigator";
-import Map from '../../features/map/Map';
+import { MapScreen } from '../../features/map/screens/MapScreen';
 import Settings from '../../features/settings/Settings';
 
 const Tab = createBottomTabNavigator();
@@ -29,7 +29,7 @@ export const AppNavigator = () => (
   <NavigationContainer>
     <Tab.Navigator screenOptions={createScreenOptions} >
       <Tab.Screen name="Restaurants" component={RestaurantsNavigator} />
-      <Tab.Screen name="Map" component={Map} />
+      <Tab.Screen name="Map" component={MapScreen} />
       <Tab.Screen name="Settings" component={Settings} />
     </Tab.Navigator>
   </NavigationContainer>
